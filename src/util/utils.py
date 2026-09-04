7ad80d3df85eee3db60c46a87645bb8d3d27d83b
G-Glitch404 <awysf1238@gmail.com>
2026-09-04 15:49:18 +0300

    added utilities

---
+++ src/util/utils.py
@@ -0,0 +1,8 @@
+ROUTES: dict[tuple, str] = {
+    ("solana", "pumpswap"): "pumpfundex",
+    ("solana", "pumpfun"): "pumpfundex",
+    ("solana", "raydium"): "solamm",
+    ("solana", "meteora"): "meteora",
+    ("base", "uniswap"): "uniswapv4",
+    ("robinhood", "uniswap"): "uniswapv4",
+}
